d7602059bfd8108357bb9343da76842c8f35e380
FahadMehmood123 <faddy.xl123@gmail.com>
2022-08-05 08:18:15 -0700

    another changes

--- src/Grandchild.js
+++ src/Grandchild.js
@@ -40,10 +40,10 @@ function Grandchild() {
         >
         
         <h2> Sign Up</h2>
-        <input id='int' type='text' placeholder='First Name' ></input>
-        <input id='int' type='text' placeholder='Last Name' ></input>
-        <input id='int' type='text' placeholder='User ID' ></input>
-        <input id='int' type='text' placeholder='Password'></input><br></br>
+        <input id='int3' type='text' placeholder='First Name' ></input>
+        <input id='int4' type='text' placeholder='Last Name' ></input>
+        <input id='int5' type='text' placeholder='User ID' ></input>
+        <input id='int6' type='text' placeholder='Password'></input><br></br>
         <input id="submit2" type='submit' value='Sign up' onClick={handleClick2}></input>
         </div>
   
